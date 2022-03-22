998b02fcbd75a3a94f767c30219509da1ea496d5
Ahmet Burak Biçer <89780902+BurakAhmet@users.noreply.github.com>
2022-03-22 10:36:31 +0300

    Update TruthTable.java

--- TruthTable.java
+++ TruthTable.java
@@ -1,40 +1,34 @@
 public class TruthTable {
 
 	public static void main(String[] args) {
-		
-		String s1="01010101";
-		String s2="10010101";
-		String AND="";
-		String OR="";
-		String XOR="";
-		for(int i=0; i<s1.length(); i++)
-		{
-			if(s1.charAt(i)==s2.charAt(i) && s1.charAt(i)=='1')
-			{
-				AND+='1';
+
+		String s1 = "10010101";
+		String s2 = "11001101";
+		String AND = "";
+		String OR = "";
+		String XOR = "";
+		if (s1.length() == s2.length()) {
+			for (int i = 0; i < s1.length(); i++) {
+				if (s1.charAt(i) == s2.charAt(i) && s1.charAt(i) == '1') {
+					AND += '1';
+				} else {
+					AND += '0';
+				}
+				if (s1.charAt(i) == '1' || s2.charAt(i) == '1') {
+					OR += '1';
+				} else {
+					OR += '0';
+				}
+				if (s1.charAt(i) != s2.charAt(i)) {
+					XOR += '1';
+				} else {
+					XOR += '0';
+				}
 			}
-			else
-			{
-				AND+='0';
-			}
-			if(s1.charAt(i)=='1' || s2.charAt(i)=='1')
-			{
-				OR+='1';
-			}
-			else
-			{
-				OR+='0';
-			}
-			if(s1.charAt(i)!=s2.charAt(i))
-			{
-				XOR+='1';
-			}
-			else
-			{
-				XOR+='0';
-			}
+			System.out.printf("AND: %s\nOR: %9s\nXOR: %s", AND, OR, XOR);
+		} else {
+			System.out.println("Please enter the string lengths equal!");
 		}
-		System.out.printf("AND: %s\nOR: %9s\nXOR: %s",AND,OR,XOR);
 	}
 
 }
